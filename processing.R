401ac924c21262ac74cfd53b861a9b9c777a494c
Damien Fung <damienfung616@gmail.com>
2022-01-16 03:47:39 -0800

    added shell script

--- processing.R
+++ processing.R
@@ -4,6 +4,7 @@ library(lubridate)
 library(dplyr)
 library(ggplot2)
 library(gganimate)
+library(gridExtra)
 
 outline_color <- "#D97C86"
 fill_color <- "#F0C0C1"
@@ -22,8 +23,9 @@ year_annotations <- list(
 
 data <- read.csv("https://health-infobase.canada.ca/src/data/covidLive/covid19-download.csv")
 provinces <- c("Canada", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "Newfoundland and Labrador", "New Brunswick", "Nova Scotia", "Prince Edward Island", "Yukon", "Northwest Territories", "Nunavut")
+plot_list <- list()
 
-make_plot <- function(province) {
+make_plot <- function(province, i) {
   daily_data <- data %>%
     filter(prname == province) %>%
     select(date, numtoday) %>%
@@ -59,16 +61,19 @@ make_plot <- function(province) {
     theme(plot.background = element_rect(color = NA, fill = "white"),
           panel.grid.major.x = element_line(color = "grey70", size = 0.8, linetype = "dotted"),
           panel.grid.minor.x = element_line(color = "grey70", size = 0.8, linetype = "dotted"),
-          axis.text.x = element_text(color = base_grey, size = 30, hjust = 0.5),) +
+          axis.text.x = element_text(color = base_grey, size = 20, hjust = 0.5),) +
     annotate("text", label = paste0(year_annotations$year, "\u2192"), x = year_annotations$x,
              y = year_annotations$y,
              size = 6, vjust = -0.6, hjust = 0.15) +
     transition_reveal(day_of_year)
-  
-  animate(data_plot, duration = 5, fps = 20, width = 1000, height = 1000, renderer = gifski_renderer())
-  anim_save(paste(province, ".gif", sep = ""), path = "./data")
+  plot_list[[i]] <- data_plot
 }
 
+i <- 1
 for (province in provinces) {
-  make_plot(province)
+  make_plot(province, i)
+  i <- i + 1
 }
+
+animate(do.call(grid.arrange, c(plot_list, ncol = 7)), duration = 5, fps = 20, width = 1000, height = 1000, renderer = gifski_renderer())
+anim_save("province_grid.gif", path = "./data")
